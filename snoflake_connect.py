9fab65a893a6f3c12ee12bc79a9cb2466bb55a25
Arpita Ghosh <34940466+arpitag1@users.noreply.github.com>
2022-11-25 16:21:13 +0000

    Update snoflake_connect.py

--- snoflake_connect.py
+++ snoflake_connect.py
@@ -20,7 +20,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from IRIS_NEW;")
 
 # Print results.
 for row in rows:
