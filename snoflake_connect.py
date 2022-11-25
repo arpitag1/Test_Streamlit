f1e64191a6920d7b1c9d9e93e5e62a074c2d380a
Arpita Ghosh <34940466+arpitag1@users.noreply.github.com>
2022-11-25 16:32:05 +0000

    Update snoflake_connect.py

--- snoflake_connect.py
+++ snoflake_connect.py
@@ -25,4 +25,4 @@ rows = run_query("SELECT * from IRIS_NEW;")
 # Print results.
 for row in rows:
     #st.write(f"{row[0]} has a :{row[1]}:")
-    st.table(rows.iloc[0:10]
+    st.table(rows.iloc[0:10])
