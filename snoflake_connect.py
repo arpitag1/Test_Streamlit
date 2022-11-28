99e14e16907eb573c9c8b19021911e39a1484cc5
Arpita Ghosh <34940466+arpitag1@users.noreply.github.com>
2022-11-28 10:47:24 +0000

    Update snoflake_connect.py

--- snoflake_connect.py
+++ snoflake_connect.py
@@ -24,9 +24,9 @@ def run_query(query):
         return cur.fetchall()
 
 rows = run_query("SELECT * from IRIS;")
-
+st.header("query run")
 #st.table(rows)
 # Print results.
 for row in rows:
     st.write(f"{row[0]} has a :{row[1]}:")
-    
+    st.header("print result")
